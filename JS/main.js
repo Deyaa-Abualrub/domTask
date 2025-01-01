1412949c25a2d7c77e22539aa067cf59c022fda5
Deyaa-Abualrub <dyaaabualrub12@gmail.com>
2025-01-02 01:14:34 +0300

    d

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+let images = [
+  "imges/download.webp",
+  "imges/download.webp",
+  "imges/download.webp",
+  "imges/download.webp",
+  "imges/download.webp",
+];
+
+let listItems = document.querySelectorAll("#fruit-list li");
+
+listItems.forEach((li, index) => {
+  let img = document.createElement("img");
+  img.src = images[index];
+  li.appendChild(img);
+});
+
+let num = prompt("Enetr Number");
+let arr = [1, 2, 12];
+
+function equal(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (num == arr[i]) 
+        return num;
+  }
+  return -1;
+}
+
+console.log(equal(arr, num));
